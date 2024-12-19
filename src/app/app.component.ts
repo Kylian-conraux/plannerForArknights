6b569698aa3112f0455f43895d80d13f60cb6934
kylian <kylianconraux@hotmail.fr>
2024-12-19 10:45:30 +0100

    ajout du sidebar au app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/layouts/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatListModule,
+    MatSidenavModule, MatToolbarModule, RouterLink, RouterLinkActive,
+    RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'plannerForArknights';
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile = true;
+  isCollapsed = true;
+
+  constructor(private observer: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if (screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  toggleMenu() {
+    if (this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false;
+    } else {
+      this.sidenav.open();
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
