03dce912b0404d84f077fd3b3c62734b1b5f09ac
kylian <kylianconraux@hotmail.fr>
2024-12-19 10:40:57 +0100

    creation du contenu de header qui permet l'ouverture/fermeture de la sidebar

--- src/app/core/components/layouts/header/header.component.ts
+++ src/app/core/components/layouts/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Output() menuToggle = new EventEmitter<void>();
 
+  onMenuToggle() {
+    this.menuToggle.emit();
+  }
 }
