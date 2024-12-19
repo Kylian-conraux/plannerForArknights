e608d326d4cb2cbefa6d88ae22a4b1b9b317d6e1
kylian <kylianconraux@hotmail.fr>
2024-12-19 13:12:18 +0100

    add operator models

---
+++ src/app/data/models/operator/operator.model.ts
@@ -0,0 +1,39 @@
+export interface Operator{
+    id: number;
+    name: string; 
+    rarity: number;
+    elite: number;
+    stats: {
+        hp: number,
+        atk: number,
+        def: number,
+        res: number,
+    }
+    skills: Skill[];
+    talents: Talent[];
+    modules: Module[];
+}
+
+export interface Skill{
+    name: string,
+    desc: string,
+    level: number
+}
+
+export interface Talent{
+    name: string,
+    desc: string,
+}
+
+export interface Module{
+    name: string,
+    talent: string,
+    level: number,
+}
+
+export interface OperatorFilters {
+    searchQuery: string;
+    rarity: number | null;
+    page: number;
+    pageSize: number;
+  }
