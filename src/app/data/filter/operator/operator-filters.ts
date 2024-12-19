33ffda36e70869f4164ba11d888dc84cfc5fdf0f
kylian <kylianconraux@hotmail.fr>
2024-12-19 13:12:31 +0100

    add operator filters

---
+++ src/app/data/filter/operator/operator-filters.ts
@@ -0,0 +1,19 @@
+import { Operator, OperatorFilters } from "../../models/operator/operator.model";
+
+export function filterOperators(operators: Operator[], filters: OperatorFilters): Operator[]{
+    let filtered = [...operators];
+
+    if(filters.searchQuery.trim()){
+        const query = filters.searchQuery.toLocaleLowerCase();
+        filtered = filtered.filter(op =>
+            op.name.toLocaleLowerCase().includes(query) //, add other way to filter the operators : class, sub class, rarity
+        );
+    }
+
+    if(filters.rarity !== null){
+        filtered = filtered.filter(op => op.rarity === filters.rarity);
+    }
+
+    return filtered;
+
+}
