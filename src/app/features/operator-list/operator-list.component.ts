fb90b37a349eaa5ce2a5365ad6d293b5c94bfed5
kylian <kylianconraux@hotmail.fr>
2024-12-19 14:14:24 +0100

    generate the feature operator list

---
+++ src/app/features/operator-list/operator-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operator-list',
+  imports: [],
+  templateUrl: './operator-list.component.html',
+  styleUrl: './operator-list.component.scss'
+})
+export class OperatorListComponent {
+
+}
