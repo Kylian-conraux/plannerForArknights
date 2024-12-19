a05e1bfd76050aed583e1f985b5382de89184992
kylian <kylianconraux@hotmail.fr>
2024-12-19 13:59:41 +0100

    adding the list of card operator

---
+++ src/app/core/components/shared/operator-list/operator-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OperatorCardComponent } from '../operator-card/operator-card.component';
+import { PaginationOperatorListComponent } from '../pagination-operator-list/pagination-operator-list.component';
+import { PaginatedResult } from '../../../../data/interface/pagination/pagination.interface';
+import { Operator } from '../../../../data/models/operator/operator.model';
+
+@Component({
+  selector: 'app-operator-list',
+  imports: [CommonModule, OperatorCardComponent, PaginationOperatorListComponent],
+  templateUrl: './operator-list.component.html',
+  styleUrl: './operator-list.component.scss'
+})
+export class OperatorListComponent {
+  @Input() paginatedResult!: PaginatedResult<Operator>;
+  @Output() pageChange = new EventEmitter<number>();
+
+}
