504e9c359512d730c6f23fc44313391cc789cf98
kylian <kylianconraux@hotmail.fr>
2024-12-19 11:26:40 +0100

    add route for home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home' //redirection in case of wrong route
+    }
+];
