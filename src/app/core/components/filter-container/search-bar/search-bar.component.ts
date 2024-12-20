383d7063285c5e537e4c3aeadbd2b1ff304a0548
kylian <kylianconraux@hotmail.fr>
2024-12-20 10:48:58 +0100

    add global styles scss to keep a single theme through the whole application; add the search bar.

--- src/app/core/components/filter-container/search-bar/search-bar.component.ts
+++ src/app/core/components/filter-container/search-bar/search-bar.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
 
+  @Input() operatorName: string = '';
+  @Output() search = new EventEmitter<string>();
+
+  get operatorNameQuerry(): string {
+    return this.operatorName;
+  }
+
+  set searchQuery(opName: string) {
+    this.operatorName = opName;
+  }
+
+  onSearch() {
+    this.search.emit(this.operatorName);
+  }
 }
