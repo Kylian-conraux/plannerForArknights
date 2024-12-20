00904144f2d09679fb6bd8fd73e133f8be1fbbdc
kylian <kylianconraux@hotmail.fr>
2024-12-20 11:25:24 +0100

    add filter container

--- src/app/core/components/filter-container/filter-container.component.ts
+++ src/app/core/components/filter-container/filter-container.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { OperatorFilters } from '../../../data/models/operator/operator.model';
+import { OperatorService } from '../../services/operator.service';
+//import {}
 
 @Component({
   selector: 'app-filter-container',
-  imports: [],
+  imports: [SearchBarComponent],
   templateUrl: './filter-container.component.html',
   styleUrl: './filter-container.component.scss'
 })
 export class FilterContainerComponent {
+
+  @Input() opFilter!: OperatorFilters;
+
+  @Output() filterChange = new EventEmitter<OperatorFilters>();
+
+  class: string[] = [];
+
+  constructor(private operatorService: OperatorService){
+    this.class = this.operatorService.getAllclass();
+  }
+
+  onSearch(query: string){
+    this.emitFilterChange({
+      ...this.opFilter,
+      searchQuery: query
+    })
+  }
+
+  private emitFilterChange(newFilters: OperatorFilters) {
+    this.filterChange.emit(newFilters);
+  }
 
 }
