f3eeaa87f7302ee65e67b5f81ae69eb0c79bd21d
Kylian-conraux <kylianconraux@hotmail.fr>
2025-01-06 14:05:00 +0100

    create component details for operator

---
+++ src/app/features/operator-details/operator-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operator-details',
+  imports: [],
+  templateUrl: './operator-details.component.html',
+  styleUrl: './operator-details.component.scss'
+})
+export class OperatorDetailsComponent {
+
+}
