1644ba90023a26d9a26266bbc9603c6de42f6efd
kylian <kylianconraux@hotmail.fr>
2025-01-08 14:08:21 +0100

    ajout d'un texte pour indiquer quelle operator est sélectionné

--- src/app/features/planner/planner.component.ts
+++ src/app/features/planner/planner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { Operator } from '../../data/models/operator/operator.model';
 import { OperatorService } from '../../core/services/operator.service';
@@ -6,20 +7,22 @@ import { FilterContainerComponent } from "../../core/components/filter-container
 import { OperatorListComponent } from '../../core/components/shared/operator-list/operator-list.component';
 import { BaseOperatorListPageComponent } from '../../core/components/shared/base-page-operator-list/base-operator-list-page.component';
 
+
 @Component({
   selector: 'app-planner',
-  imports: [OperatorListComponent, FilterContainerComponent],
+  imports: [OperatorListComponent, FilterContainerComponent, CommonModule],
   templateUrl: './planner.component.html',
   styleUrl: './planner.component.scss'
 })
 export class PlannerComponent extends BaseOperatorListPageComponent {
 
+  operator: Operator | undefined = undefined;
 
   constructor(operatorService: OperatorService) {
     super(operatorService);
   }
 
   onTileClick(operator: Operator): void {
-
+    this.operator = operator;
   }
 }
