74db0dda0c95e5771d5a0a46958248ef480f6324
kylian <kylianconraux@hotmail.fr>
2025-01-08 13:38:46 +0100

    added a super component to be extended to both planner page and operator list page and make onTileClick an abstract class to adapt his utility

---
+++ src/app/core/components/shared/base-page-operator-list/base-operator-list-page.component.ts
@@ -0,0 +1,146 @@
+import { OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Operator, OperatorFilters } from '../../../../data/models/operator/operator.model';
+import { PaginatedResult } from '../../../../data/interface/pagination/pagination.interface';
+import { OperatorService } from '../../../services/operator.service';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-operator-list-page',
+  template: ''
+})
+/**
+ * Composant de base pour la gestion des opérateurs dans Arknights.
+ * Ce composant est abstrait et doit être étendu par d'autres composants.
+ * Il gère le chargement des opérateurs et la pagination.
+ * 
+ * @abstract
+ * @implements {OnInit}
+ * @implements {OnDestroy}
+ */
+export abstract class BaseOperatorListPageComponent implements OnInit, OnDestroy {
+
+  /**
+   * Liste des opérateurs chargés.
+   * @type {Operator[]}
+   */
+  operators: Operator[] = [];
+
+  /**
+   * Subscription pour gérer les abonnements aux observables.
+   * @private
+   * @type {Subscription}
+   */
+  private subscription: Subscription = new Subscription();
+
+  /**
+   * Filtres appliqués pour la recherche et la pagination des opérateurs.
+   * @type {OperatorFilters}
+   */
+  filters: OperatorFilters = {
+    searchQuery: '',
+    rarity: null,
+    page: 1,
+    pageSize: 10
+  };
+
+  /**
+   * Résultat paginé des opérateurs.
+   * @type {PaginatedResult<Operator>}
+   */
+  paginatedResult: PaginatedResult<Operator> = {
+    items: [],
+    total: 0,
+    currentPage: 1,
+    totalPages: 1,
+    hasNext: false,
+    hasPrevious: false
+  };
+
+  /**
+   * Constructeur du composant.
+   * @param {OperatorService} operatorService - Service pour gérer les opérateurs.
+   */
+  constructor(private operatorService: OperatorService) { }
+
+  /**
+   * Méthode appelée lors de l'initialisation du composant.
+   * Charge les opérateurs et les résultats paginés.
+   */
+  ngOnInit(): void {
+    this.loadOperator();
+    this.loadOperators();
+  }
+
+  /**
+   * Méthode appelée lors de la destruction du composant.
+   * Désabonne de toutes les subscriptions.
+   */
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  /**
+   * Charge la liste des opérateurs.
+   * @private
+   */
+  private loadOperator() {
+    this.operatorService.getOperators().subscribe({
+      next: (data) => {
+        this.operators = data;
+      },
+      error: (err) => {
+        console.error('Failed to load operators:', err);
+      }
+    });
+  }
+
+  /**
+   * Charge les opérateurs paginés en fonction des filtres appliqués.
+   * @private
+   */
+  private loadOperators() {
+    this.operatorService.getPaginatedoperators(this.filters).subscribe({
+      next: (data) => {
+        this.paginatedResult = data;
+      },
+      error: (err) => {
+        console.error('Failed to load paginated operators:', err);
+      }
+    });
+  }
+
+  /**
+   * Méthode abstraite à implémenter pour gérer le clic sur une tuile d'opérateur.
+   * @abstract
+   * @param {Operator} operator - L'opérateur sur lequel on a cliqué.
+   */
+  abstract onTileClick(operator: Operator): void;
+
+  /**
+   * Méthode appelée lors du changement des filtres.
+   * Met à jour les filtres et recharge les opérateurs paginés.
+   * @param {OperatorFilters} newFilters - Les nouveaux filtres appliqués.
+   */
+  onFiltersChange(newFilters: OperatorFilters) {
+    this.filters = {
+      ...newFilters,
+      page: 1
+    };
+    this.loadOperators();
+  }
+
+  /**
+   * Méthode appelée lors du changement de page.
+   * Met à jour la page actuelle et recharge les opérateurs paginés.
+   * @param {number} page - Le numéro de la nouvelle page.
+   */
+  onPageChange(page: number) {
+    this.filters = {
+      ...this.filters,
+      page
+    };
+    this.loadOperators();
+  }
+}
--- src/app/features/operator-list/operator-list-page.component.ts
+++ src/app/features/operator-list/operator-list-page.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
-import { Operator, OperatorFilters } from '../../data/models/operator/operator.model';
-import { PaginatedResult } from '../../data/interface/pagination/pagination.interface';
+import { Operator} from '../../data/models/operator/operator.model';
 import { OperatorService } from '../../core/services/operator.service';
 import { OperatorListComponent } from '../../core/components/shared/operator-list/operator-list.component';
 import { FilterContainerComponent } from "../../core/components/filter-container/filter-container.component";
+import { BaseOperatorListPageComponent } from '../../core/components/shared/base-page-operator-list/base-operator-list-page.component';
 
 
 @Component({
@@ -15,64 +14,13 @@ import { FilterContainerComponent } from "../../core/components/filter-container
   templateUrl: './operator-list-page.component.html',
   styleUrl: './operator-list-page.component.scss'
 })
-export class OperatorListComponentPage implements OnInit {
+export class OperatorListComponentPage extends BaseOperatorListPageComponent implements OnInit {
 
 
- // Liste des opérateurs chargés
-  operators: Operator[] = [];
-
-  // Subscription pour gérer les flux d'observables et éviter les fuites de mémoire
-  private subscription: Subscription = new Subscription();
-
-
-  // Filtres appliqués à la liste des opérateurs
-  filters: OperatorFilters = {
-    searchQuery: '',
-    rarity: null,
-    page: 1,
-    pageSize: 10
-  };
-
-   // Résultat paginé des opérateurs
-  paginatedResult: PaginatedResult<Operator> = {
-    items: [],
-    total: 0,
-    currentPage: 1,
-    totalPages: 1,
-    hasNext: false,
-    hasPrevious: false
-  };
-
-
-  constructor(private operatorService: OperatorService,
+  constructor(operatorService: OperatorService,
     private route: Router
   ) {
-
-  }
-
-  ngOnInit(): void {
-
-    this.loadOperator();// Charge la liste simple des opérateurs
-    this.loadOperators();// Charge la liste paginée des opérateurs
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  /**
-   * Charge une liste d'opérateurs sans pagination
-   */
-  private loadOperator() {
-    this.operatorService.getOperators().subscribe({
-      next: (data) => {
-        this.operators = data;
-      },
-      error: (err) => {
-        console.error('Failed to load operators:', err);
-      }
-    });
-
+    super(operatorService);
   }
 
 
@@ -80,47 +28,10 @@ export class OperatorListComponentPage implements OnInit {
    * Navigue vers la page d'un opérateur spécifique
    * @param operator L'opérateur cliqué
    */
-   onTileClick(operator: Operator): void {
+  onTileClick(operator: Operator): void {
     this.route.navigate(['/operator', operator.id]);
   }
 
 
-  /**
-   * Met à jour les filtres et recharge la liste paginée des opérateurs
-   * @param newFilters Les nouveaux filtres à appliquer
-   */
-  onFiltersChange(newFilters: OperatorFilters) {
-    this.filters = {
-      ...newFilters,// Fusionne les nouveaux filtres avec les filtres existants
-      page: 1
-    };
-    this.loadOperators();
-  }
 
-  /**
-   * Change la page active et recharge les opérateurs
-   * @param page Numéro de la nouvelle page
-   */
-  onPageChange(page: number) {
-    this.filters = {
-      ...this.filters,
-      page
-    };
-    this.loadOperators();
-  }
-
-   /**
-   * Charge les opérateurs en fonction des filtres appliqués avec pagination
-   */
-  private loadOperators() {
-    this.operatorService.getPaginatedoperators(this.filters).subscribe({
-      next: (data) => {
-        this.paginatedResult = data;
-      },
-      error: (err) => {
-        console.error('Failed to load paginated operators:', err);
-      }
-    });
-  }
-  
 }
