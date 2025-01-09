a5f4c288caaa86dea962a68f8157f896d77dcb5d
kylian <kylianconraux@hotmail.fr>
2025-01-09 13:48:49 +0100

    adding the progression component to the planner

--- src/app/features/planner/planner.component.ts
+++ src/app/features/planner/planner.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { Operator } from '../../data/models/operator/operator.model';
 import { OperatorService } from '../../core/services/operator.service';
 import { FilterContainerComponent } from "../../core/components/filter-container/filter-container.component";
 import { OperatorListComponent } from '../../core/components/shared/operator-list/operator-list.component';
 import { BaseOperatorListPageComponent } from '../../core/components/shared/base-page-operator-list/base-operator-list-page.component';
-
+import { ProgressionConfiguratorComponent } from '../../core/components/shared/planner/progression-configurator/progression-configurator.component';
 
 @Component({
   selector: 'app-planner',
-  imports: [OperatorListComponent, FilterContainerComponent, CommonModule],
+  imports: [OperatorListComponent, FilterContainerComponent, CommonModule, ReactiveFormsModule, ProgressionConfiguratorComponent],
   templateUrl: './planner.component.html',
   styleUrl: './planner.component.scss'
 })
@@ -18,11 +19,25 @@ export class PlannerComponent extends BaseOperatorListPageComponent {
 
   operator: Operator | undefined = undefined;
 
-  constructor(operatorService: OperatorService) {
+  formGroup!: FormGroup;
+
+  constructor(operatorService: OperatorService, private fBuilder: FormBuilder) {
     super(operatorService);
+    this.formGroup = this.fBuilder.group({
+      level: [1],
+      elite: [0],
+      skill: [1],
+      levelToReach: [90],
+      eliteToReach: [2],
+      skillToReach: [7]
+    });
   }
 
   onTileClick(operator: Operator): void {
     this.operator = operator;
+  }
+
+  onFormChange(event: Event): void {
+    console.log('form changed', event);
   }
 }
